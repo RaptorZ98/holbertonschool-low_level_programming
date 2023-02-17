d6285b639c51a63bc05632724c7f6dd3930523fe
RaptorZ98 <federicoheuercor@hotmail.com>
2023-02-17 05:30:38 -0800

    added script 2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,17 @@
 #include <stdio.h>
 
 /**
- *
+ * main - prints the alphabet
+ * Description: prints the alphabet only using printchar
+ * Return: the correct return is 0
 */
 
 int main(void)
 {
-	
+	char c;
 
+	for (c = 'a'; c <= 'z'; c++)
+		putchar(c);
+	putchar('\n');
+	return (0);
 }

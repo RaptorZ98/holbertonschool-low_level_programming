708b4c9cccd0f5c40f7567d530866044a9743a65
RaptorZ98 <federicoheuercor@hotmail.com>
2023-02-16 16:28:32 -0800

    added script 5

---
+++ hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - uses printf to print something
+ * description - uses printf to print what was asked
+ * Return: the correct retured value is 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}

61e1a44b641022bb4a4433ea5b19614c3ff4257c
RaptorZ98 <federicoheuercor@hotmail.com>
2023-02-17 04:50:13 -0800

    added script 1

---
+++ variables_if_else_while/1-last_digit.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Tells you if a random number is greater 5
+ * description: random number is greater han 5 o less than 5
+ * Return: The correct return of the program is 0
+*/
+
+int main(void)
+{
+	int n;
+	int nc;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	nc = n % 10;
+	if (nc > 5)
+	{
+		printf("Last digit of %d and is greater than 5", n);
+	}
+	else if (nc < 6 && nc != 0)
+	{
+		printf("Last digit of %d and is less than 6 and not 0", n);
+	}
+	else
+	{
+		printf("Last digit of %d and is 0", n);
+	}
+	return (0);
+}

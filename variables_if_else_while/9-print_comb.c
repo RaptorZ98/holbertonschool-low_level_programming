1b74635e55552d1907c30d6db7546e9b064cc9a1
RaptorZ98 <federicoheuercor@hotmail.com>
2023-02-17 07:27:20 -0800

    added script 9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints all single char num with ,
+ * Description: prints all single char num with , putchar
+ * Return: the correct return is 0
+*/
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		putchar(n);
+		if (n < 9)
+		{
+			putchar('.');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}

0cf59205e9aaaee0f3f21029efe3c5174d1595a4
RaptorZ98 <federicoheuercor@hotmail.com>
2023-02-17 16:31:43 -0800

    mistakes

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
-#include "main.h"
+#include "_putchar.c"
 /**
  * main - prints putchar
  * Description: prints _putchar uising _putchar
  * Return: the correct return is 0
 */
 
-int main(void)
+int perro(void)
 {
 	char c[] = {'_', 'p', 'u', 't', 'c', 'h', 'a', 'r', '\n'};
 	int n = 0;
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,4 +1,10 @@
-#include <unistd.h>
-#include "_putchar.c"
+#ifndef main.h
+#define main.h
 
+#include "_putchar.c"
+#include "0-putchar.c"
+
+
+int perro(void);
 int _putchar(char c);
+#endif

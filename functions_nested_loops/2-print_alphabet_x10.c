2fa16389cfd91d0e3a9dbb40bf88b2cc5df0da41
RaptorZ98 <federicoheuercor@hotmail.com>
2023-02-18 19:27:33 -0800

    added script 2

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet
+ *
+ * Return: the return is void
+*/
+
+void print_alphabet_x10(void)
+{
+	char c;
+	int n;
+	n = 1;
+
+	while (n <= 10)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+			_putchar(c);
+		_putchar('\n');
+		n++;
+	}
+}

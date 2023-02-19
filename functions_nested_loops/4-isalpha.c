1fac2be6a8a4c2a764f4e9d3e22d5ea1c030cc49
RaptorZ98 <federicoheuercor@hotmail.com>
2023-02-19 06:34:35 -0800

    added script 4

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks if lowercase
+ * Return: 1 if it is lowercase 0 if not
+ * @c: parameter
+*/
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+		return (1);
+	return (0);
+}

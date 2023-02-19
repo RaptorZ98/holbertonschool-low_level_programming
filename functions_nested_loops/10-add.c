94c337feaf070175e320aa4c6bba8891d4ec207f
RaptorZ98 <federicoheuercor@hotmail.com>
2023-02-19 08:55:08 -0800

    added script 10

--- functions_nested_loops/10-add.c
+++ functions_nested_loops/10-add.c
@@ -2,9 +2,9 @@
 
 /**
  * add - adds two integers
- * Return - the result
- * @n - first parameter
- * @m - second parameter
+ * Return: - the result
+ * @n: first parameter
+ * @m: second parameter
  */
 
 int add(int n, int m)

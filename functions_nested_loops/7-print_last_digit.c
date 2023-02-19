a62d32282ac6be36590bafceba9e3efbc712f918
RaptorZ98 <federicoheuercor@hotmail.com>
2023-02-19 07:32:27 -0800

    added mistke corretct

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -10,21 +10,16 @@ int print_last_digit(int n)
 {
 	int m;
 
-	if (n > 0)
+	m = n % 10;
+	if (m < 0);
 	{
-		m = n % 10;
-		_putchar('0' + m);
-		return (m);
-}
-	else if (n < 0)
-	{
-		m = n % -10;
+		m = -m;
 		_putchar('0' + m);
 		return (m);
 	}
 	else
 	{
-		_putchar('0' + 0);
-		return (0);
+		_putchar('0' + m);
+		return (m);
 	}
 }

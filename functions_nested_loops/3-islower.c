65460e104ce9aa1d02a8f5641af8418359fc534b
RaptorZ98 <federicoheuercor@hotmail.com>
2023-02-19 06:26:31 -0800

    mistake123432

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -4,11 +4,13 @@
  * _islower - checks if lowercase
  *
  * Return: return 1 if lowercase 0 otherwise
+ * Parameter: the parameter is an int c wich is the character to lower
+ * @c: parameter
 */
 
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
-		return (1);	
+		return (1);
 	return (0);
 }

7038c5c543c3cf184b5836d359e4c60b042d4b48
RaptorZ98 <federicoheuercor@hotmail.com>
2023-02-19 07:22:20 -0800

    mistake

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -12,19 +12,19 @@ int print_last_digit(int n)
 
 	if (n > 0)
 	{
-		m = n % 10;
-		_putchar(m + '0');
+		m = (n % 10);
+		_putchar('0' + m);
 		return (m);
 	}
 	else if (n < 0)
 	{
-		m = -n % 10;
-		_putchar(m + '0');
+		m = (-n % 10);
+		_putchar('0' + m);
 		return (m);
 	}
 	else
 	{
-		_putchar(0 + '0');
+		_putchar('0' + 0);
 		return (0);
 	}
 }

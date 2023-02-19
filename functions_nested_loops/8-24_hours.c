ff570f15dc5e5ab109fec0057f8c2db85748fe36
RaptorZ98 <federicoheuercor@hotmail.com>
2023-02-19 07:43:13 -0800

    added script 8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * jack_bauer - prints every minute 
+ *Return: not
+*/
+
+void jack_bauer(void)
+{
+	int hh;
+	int min;
+
+	hh = 0;
+	min = 0;
+	while (hh != 24)
+	{
+		_putchar((hh / 10) + '0');
+		_putchar((hh % 10) + '0');
+		_putchar(':');
+		_putchar((min / 10) + '0');
+		_putchar((min % 10) + '0');
+		_putchar('\n');
+		if (min < 59)
+			min++;
+		else
+		{
+			min = 0;
+			hh++;
+		}
+	}
+}

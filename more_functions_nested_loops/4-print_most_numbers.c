d7d7f016b14c16d4722bc055ffffebcbd3bf3b27
RaptorZ98 <federicoheuercor@hotmail.com>
2023-02-22 07:04:42 -0800

    added script 4

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers 0 to 9 except 2 and 4
+ * Return: void
+ */
+
+void print_most_numbers(void)
+{
+	int c;
+
+	for (c >= '0'; c <= '9'; c++)
+		if (c != '4' || c != '2')
+			_putchar(c);	
+}

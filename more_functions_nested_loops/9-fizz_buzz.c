6312367d9b9449702102e63883356633bd7b07f8
RaptorZ98 <federicoheuercor@hotmail.com>
2023-02-22 15:51:37 -0800

    added script 9

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints 1 to 100
+ * Return: void
+ */
+
+void main(void)
+{
+	int n;
+	int fuz;
+	int buz;
+
+	for (n = 1; n <= 100; n++)
+	{
+		fuz = n % 3;
+		buz = n % 5;
+		if (fuz == 0 && buz == 0)
+		{
+			prtinf("FizzBuzz ");
+		}
+		else if (fuz == 0)
+		{
+			pritnf("Fizz ");
+		}
+		else if (buz == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+	}
+}

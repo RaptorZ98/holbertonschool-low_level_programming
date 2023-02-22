31abdf075b6b8b67ff0cf18dbf472b32e6ccbbb7
RaptorZ98 <federicoheuercor@hotmail.com>
2023-02-22 15:36:01 -0800

    added script 8

--- more_functions_nested_loops/8-print_square.c
+++ more_functions_nested_loops/8-print_square.c
@@ -13,9 +13,9 @@ void print_square(int size)
 
 	if (size > 0)
 	{
-		for (a = 1; a <= n; a++)
+		for (a = 1; a <= size; a++)
 		{
-			for (b = 1; b <= n; b++)
+			for (b = 1; b <= size; b++)
 				_putchar('#');
 			_putchar('\n');
 		}

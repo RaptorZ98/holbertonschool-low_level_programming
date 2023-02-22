52a7fbf7a1c7c0f241f627ece17ed26eb06fa0f9
RaptorZ98 <federicoheuercor@hotmail.com>
2023-02-22 07:26:20 -0800

    added script 5

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 10 times the numbers
+ * Return: void
+ */
+
+void more_numbers(void)
+{
+	for (a = 0; a <= 10; a++)
+		for (b = 0; b <= 14; b++)
+			_putchar(b + '0');
+		_putchar('\n');
+	_putchar('\n');
+}

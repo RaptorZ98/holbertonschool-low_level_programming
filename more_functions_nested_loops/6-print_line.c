ca45cddc35c54edf36b7c030d2ecd28ca2a72755
RaptorZ98 <federicoheuercor@hotmail.com>
2023-02-22 08:39:52 -0800

    added script 6

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ * Return: void
+ * @n: parameter
+ */
+
+void print_line(int n)
+{
+	int a;
+	
+	if (n >= 0)
+	{
+		for (a = 0; a <= n; a++)
+			_puthcar('_');
+	{
+	else
+	{
+		_putchar('\n');
+	}
+}

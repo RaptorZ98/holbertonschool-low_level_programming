f9a51dec2677e784d05060fb08b8184d14d2e1da
RaptorZ98 <federicoheuercor@hotmail.com>
2023-02-22 08:47:16 -0800

    am6.2

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -10,10 +10,14 @@ void print_line(int n)
 {
 	int a;
 	
-	if (n >= 0)
+	if (n > 0)
 	{
 		for (a = 0; a <= n; a++)
 			_putchar('_');
+		_putchar('\n');
+	}
+	if else (n = 0)
+	{
 		_putchar('\n');
 	}
 	else

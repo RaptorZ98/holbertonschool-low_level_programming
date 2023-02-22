2199b7cca264b2504b05a243733c000fea80ac5f
RaptorZ98 <federicoheuercor@hotmail.com>
2023-02-22 06:40:34 -0800

    added script 2

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * Return: the result
+ * @a: parameter 1
+ * @b: parameter 2
+ */
+
+int mul(int a, int b)
+{
+	int sum;
+
+	sum = a * b;
+	return (sum);
+}

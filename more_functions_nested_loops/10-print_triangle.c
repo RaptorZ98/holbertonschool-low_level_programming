e05a15abb7968d4f2d773b28c12c80e544ee9314
RaptorZ98 <federicoheuercor@hotmail.com>
2023-02-22 16:20:07 -0800

    added mistake correction

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -14,9 +14,9 @@ void print_triangle(int size)
 
 	if (size > 0)
 	{
-		for (a = size; a > 1; a--)
+		for (a = size; a > 0; a--)
 		{
-			for (b = 1; b <= a; b++)
+			for (b = 1; b < a; b++)
 				_putchar(' ');
 			for (c = size; c >= a; c--)
 				_putchar('#');

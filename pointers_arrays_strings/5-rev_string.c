ca84db7ea87e34a00168d3b8f0c379925d70bfdb
RaptorZ98 <federicoheuercor@hotmail.com>
2023-02-28 03:19:13 -0800

    added incomplete script 5

---
+++ pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * rev-string - prints string in reverse
+ * @s: parameter
+ * Reutrn: void
+ */
+
+void rev-string(char *s)
+{
+	int a;
+	int b;
+
+	b = 0;
+	for (a = 0; *(s + a) != '\0'; a++)
+	{
+	}
+	for (a = a - 1; a >= 0; a--)
+	{
+		*(s + b) = 
+		b++;
+	}	
+}

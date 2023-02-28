b1814f5dfb4fb7980b0f4513c2085c533bb865f5
RaptorZ98 <federicoheuercor@hotmail.com>
2023-02-28 02:56:25 -0800

    added script 2

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - return lenght of a string
+ * @s: parameter
+ * Return: int
+ */
+
+int _strlen(char *s)
+{
+	int n;
+
+	for (n = 0; *(s + n) != '\0'; n++)
+	return (n);
+}

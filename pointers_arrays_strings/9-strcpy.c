f1faf017ad34921900cabb027df7537b389f29ac
RaptorZ98 <federicoheuercor@hotmail.com>
2023-02-28 08:21:47 -0800

    added script 9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strcpy - return the pointer of dest
+ * @dest: parameter 1
+ * @src: parameter 2
+ * Return: char
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int n;
+
+	for (n = 0; *(src + n) != '\0'; n++)
+		*(dest + n) = *(src + n);
+	*(dest + n) = *(src + n);
+	return = (dest);
+}

f5da431af494469c2791edcb5db3d7a15a75b33a
RaptorZ98 <federicoheuercor@hotmail.com>
2023-02-28 06:30:40 -0800

    added script 8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -16,9 +16,10 @@ void print_array(int *a, int n)
 	{
 		if (b == n - 1)
 		{
-			printf("%d\n", *(a + b));
+			printf("%d", *(a + b));
 		}
 		else
 			printf("%d, ", *(a + b));
 	}
+	_putchar('\n');
 }

b61cd9ee93e3fe4bf6a41043a62cc3bb14de59c4
RaptorZ98 <federicoheuercor@hotmail.com>
2023-02-28 09:09:43 -0800

    added script 0 2

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: parameter 1
+ * @src: parameter 2
+ * Return: char
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int n;
+	int m;
+
+	for (n = 0; *(dest + n) != '\0'; n++)
+	{
+	}
+	for (b = 0; *(src + b) != '\0'; b++)
+	{
+		*(dest + n) = *(src + b);
+		n++;
+	}
+	n++;
+	*(dest + n) = *(src + b)
+	return (dest); 
+}
---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: parameter
+ * Return: return the int
+ */
+
+int _atoi(char *s)
+{
+
+}

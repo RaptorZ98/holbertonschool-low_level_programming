9073b44059d1d6c97033510030cf38c565bd2763
RaptorZ98 <federicoheuercor@hotmail.com>
2023-02-28 05:44:38 -0800

    mistake 7

--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -14,16 +14,7 @@ void puts_half(char *str)
 	for (a = 0; *(str + a) != '\0'; a++)
 	{
 	}
-	if (a % 2 == 0)
-	{
-		for (b = a / 2; b < a; b++)
-			_putchar(*(str + b));
-		_putchar('\n');
-	}
-	else
-	{
-		for (b = (a - 1) / 2; b < a; b++)
-			_putchar(*(str + b));
-		_putchar('\n');
-	}
+	for (b = a / 2; b < a; b++)		
+		_putchar(*(str + b));
+	_putchar('\n');
 }

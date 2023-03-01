08e7c88f55c452425773659ae86a07473818b90c
RaptorZ98 <federicoheuercor@hotmail.com>
2023-02-28 17:58:59 -0800

    added script 2 2

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -9,5 +9,23 @@
 
 char *_strncpy(char *dest, char *src, int n)
 {
+	int m;
+	int d;
 
+	m = 0;
+	while (*(src + m) != '\0')
+		m++;
+	if (m >= n)
+	{
+		for (d = 0; d < n; d++)
+			*(dest + d) = *(src + d);
+	}
+	else
+	{
+		for (d = 0; d <= m - 1; d++)
+			*(dest + d) = *(src + d);
+		for (; d < n; d++)
+			*(dest + d) = '\0';
+	}
+	return (dest);
 }

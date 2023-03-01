dee1c7b10a84460b408d74d5e60f06ef333ef46d
RaptorZ98 <federicoheuercor@hotmail.com>
2023-02-28 19:58:15 -0800

    added script 5

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -13,7 +13,7 @@ char *string_toupper(char *n)
 	p = 0;
 	while (*(n + p) != '\0')
 	{
-		if ((*(n + p) >= 'a') && (*(n + p) <+ 'z'))
+		if ((*(n + p) >= 'a') && (*(n + p) <= 'z'))
 			*(n + p) -= 32;
 		p++;
 	}

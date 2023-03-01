2d2e98a03817100b00339f0a7cd35198f751304f
RaptorZ98 <federicoheuercor@hotmail.com>
2023-03-01 09:26:45 -0800

    added incomplete script 3 3

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strspn - gets the length of a prefix
+ * @s: parameter 1
+ * @accept: parameter 2
+ * Return: int
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+
+}

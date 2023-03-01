1562550e8081a5814493c523d636284d78b526c3
RaptorZ98 <federicoheuercor@hotmail.com>
2023-03-01 08:21:00 -0800

    added script 0 3

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _memeset - fills a memory with a constant byte
+ * @s: parameter 1
+ * @b: parameter 2
+ * @n: parameter 3
+ * Return: char
+ */
+
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int d;
+
+	for (d = 0; d < n; d++)
+	{
+		*(s + d) = b;
+	}
+	return (s);
+}

d12154f9b0054fa580c8cc447fe75ffdc87f2c07
RaptorZ98 <federicoheuercor@hotmail.com>
2023-03-01 08:41:27 -0800

    added script 1

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _memcpy - copies memory area
+ * @dest: parameter 1
+ * @src: parameter 2
+ * @n: parameter 3
+ * Return: char
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int m;
+
+	for (m = 0; m < m; m++)
+		*(dest + m) = *(src + m);
+	return (dest);
+}

f695d78ee54ce3a42725d23bc9e44cdeea3e24f6
RaptorZ98 <federicoheuercor@hotmail.com>
2023-03-01 06:31:39 -0800

    added script 7 2

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * leet - encodes 1337
+ * @m: parameter
+ * Return: char
+ */
+
+
+char *leet(char *m)
+{
+	int n;
+	int d;
+	char a[5] = {'a', 'e', 'o', 't', 'l'};
+	char b[5] = {'4', '3', '0', '7', '1'};
+
+	n = 0;
+	while (*(m + n) != '\0')
+	{
+		for (d = 0; d < 5; d++)
+			if (*(m + n) == *(a + d) || *(m + n) == (*(m + n) - 32))
+				*(m + n) = *(b + d);
+		n++;
+	}
+	return (m);
+}

2b7226a3098ff0c46d79674bf3c21bc1110dc9e1
RaptorZ98 <federicoheuercor@hotmail.com>
2023-03-01 09:19:58 -0800

    added script 2

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -18,15 +18,15 @@ char *_strchr(char *s, char c)
 	d = 0;
 	while (i == 0)
 	{
-		if (s[d] != '\0' && s[d] != c)
+		if ((*(s + d) != '\0') && (*(s + d) != c))
 			d++;
-		else if (s[d] == '\0')
+		else if (*(s + d) == '\0')
 			i = 1;
-		else if (s[d] == c)
+		else if (*(s + d) == c)
 			i = 2;
 	}
 	if (i == 2)
-		return (&(s[d]));
+		return (&(*(s + d)));
 	else
 		return (NULL);
 }

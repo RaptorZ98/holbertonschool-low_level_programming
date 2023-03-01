626b8a613ae98c390dbe224a2c67df1ef84a43ea
RaptorZ98 <federicoheuercor@hotmail.com>
2023-02-28 16:18:30 -0800

    add

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string
+ * @dest: parameter 1
+ * @src: parameter 2
+ * @n: parameter 3
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+
+}

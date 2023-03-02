5dc70f33a871c149c2f14b8df8597a0c94d52b33
RaptorZ98 <federicoheuercor@hotmail.com>
2023-03-01 17:32:24 -0800

    added script 5

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * _strstr - locates a substring
+ * @haystack: parameter 1
+ * @needle: parameter 2
+ * Return: char
+ */
+
+char *_strstr(char *haystack, char *needle)
+{
+	int n;
+	int m;
+	int t;
+
+	n = 0
+	while (haystack[n] != '\0')
+	{
+		t = n;
+		if (haystack[t] == needle[0])
+		{
+			for (m = 0; needle[m] == haystack[t] && needle[m] != '\0'; m++)
+			{
+				t++;
+			}
+			if (needle[m] == '\0')
+				return (&haystack[n]);
+		}
+		n++;
+	}
+	return ('\0');
+}

67d80e38bd6f94b4e336bc821d2a077662751038
RaptorZ98 <federicoheuercor@hotmail.com>
2023-03-01 16:29:44 -0800

    added mistake 123123

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -13,13 +13,20 @@ unsigned int _strspn(char *s, char *accept)
 	int d;
 	unsigned int r;
 
-	r =0;
+	r = 0;
 	n = 0;
 	while (*(s + n) != '\0')
 	{
 		for (d = 0; *(accept + d) != '\0'; d++)
 			if (*(s + n) == (*(accept + d)))
-					r++;
+			{
+				r++;
+			}
+			else if (*(s + n) == ' ' || (c[i] == '\t') || (c[i] == '\n')
+			|| (c[i] == ',') || (c[i] == ';') || (c[i] == '.') || (c[i] == '!')
+			|| (c[i] == '?') || (c[i] == '"') || (c[i] == '(') || (c[i] == ')')
+			|| (c[i] == '{') || (c[i] == '}'))
+				return (r);
 		n++;
 	}
 	return (r);

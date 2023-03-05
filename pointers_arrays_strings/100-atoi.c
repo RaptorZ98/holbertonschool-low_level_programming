5917cfc227ad0d42b455435fc58de30af51f26b0
RaptorZ98 <federicoheuercor@hotmail.com>
2023-03-04 19:21:01 -0800

    added script 10

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -9,9 +9,20 @@
 int _atoi(char *s)
 {
 	int i;
+	int d;
+	int c;
 
-	while(i == 0)
+	i = 1;
+	c = 0;
+	while (s[c])
 	{
-
+		if (s[c] == '-')
+			i = c * -1;
+		else if (s[c] >= 48 && s[c] <= 57)
+			d = s[c] - '0';
+			d = i * d;
+			return (d);
+		c++;
 	}
+	return (0);
 }

8d5f152e9d55a31dc7feeacd49b6b194eddb7750
RaptorZ98 <federicoheuercor@hotmail.com>
2023-03-06 13:21:07 -0800

    added script 1

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -1,6 +1,6 @@
 #include "main.h"
 
-/*
+/**
  * _print_rev_recursion - prints a string in reverse
  * @s: parameter 1
  * Return: void
@@ -12,5 +12,5 @@ void _print_rev_recursion(char *s)
 	{
 		_print_rev_recursion(&(s[1]));
 		_putchar(s[0]);
-	}	
+	}
 }

5e7336870d1b977bd5c65f464eff2c01872a4cc4
RaptorZ98 <federicoheuercor@hotmail.com>
2023-03-06 13:29:29 -0800

    added script 2

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - return the length of a string
+ * @s: parameter 1
+ * Return: int
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (s[0] != '\0')
+		return ((1 + _strlen_recursion(char *s);
+	return (1);
+}

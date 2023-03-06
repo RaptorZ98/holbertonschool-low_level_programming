208689d0592a5873bf427ad9ab50dbcbaec8ba3d
RaptorZ98 <federicoheuercor@hotmail.com>
2023-03-06 15:17:26 -0800

    added script 4

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _pow_recurison - value of x raised to the power of y
+ * _pow_recursion - value of x raised to the power of y
  * @x: parameter 1
  * @y: parameter 2
  * Return: int
@@ -12,7 +12,7 @@ int _pow_recursion(int x, int y)
 	if (y < 0)
 		return (-1);
 	else if (y > 0)
-		return (x * _pow_recursion(x, (y - 1)));
+		return ((x * _pow_recursion(x, (y - 1))));
 	else
-		return 0;
+		return (1);
 }

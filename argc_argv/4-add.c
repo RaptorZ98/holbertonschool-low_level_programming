868549930bf0146b89a15ee8f6e875336b98880a
RaptorZ98 <federicoheuercor@hotmail.com>
2023-03-08 09:04:52 -0800

    wrong script

---
+++ argc_argv/4-add.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: parameter 1
+ * @argv: parameter 2
+ * Return: int
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+	int m;
+
+	if (argc < 1)
+		printf("0\n");
+	for(
+		for (i = 0; i < argc; i++)
+		{
+			if ()
+		}
+}

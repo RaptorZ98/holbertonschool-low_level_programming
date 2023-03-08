f895609e580fa6386b47a5b37e1781aa43945512
RaptorZ98 <federicoheuercor@hotmail.com>
2023-03-08 08:33:23 -0800

    added script 3

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - multiplies two numbers
+ * @argc: parameter 1
+ * @argv: parameter 2
+ * Return: int
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc == 3)
+	{
+		printf("%d\n", argv[1] * argv[2]);
+		return (0);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+}

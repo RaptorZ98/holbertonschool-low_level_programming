8a96001ecaa8d0ca06fcd100351d91e006b2a9f5
RaptorZ98 <federicoheuercor@hotmail.com>
2023-03-07 18:22:36 -0800

    added script 0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints program name 
+ * @argc: parameter 1
+ * @argv: parameter 2
+ * Return: void
+ */
+
+void main(int argc, char* argv)
+{
+	printf("%s\n", argv[0]);
+}

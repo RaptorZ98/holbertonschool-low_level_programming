5c9b74eb09186d0c67a93c2dcb50e948c8eb6f50
RaptorZ98 <federicoheuercor@hotmail.com>
2023-03-08 08:18:54 -0800

    added script 2

---
+++ argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - prints all arguments
+ * @argc: parameter 1
+ * @argv: parameter 2
+ * Return: int
+ */
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	for (i = 0, i < argc, i++)
+		printf("%s\n", argv[i]);
+	return (0);
+}	

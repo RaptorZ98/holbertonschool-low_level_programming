a248b72cb1443d57bc288ce936bb1decd858b15e
RaptorZ98 <federicoheuercor@hotmail.com>
2023-03-08 08:38:47 -0800

    added script 3

--- argc_argv/3-mul.c
+++ argc_argv/3-mul.c
@@ -10,9 +10,14 @@
 
 int main(int argc, char **argv)
 {
+	int n;
+	int m;
+
+	n = argv[1] + '0';
+	m = argv[2] + '0';
 	if (argc == 3)
 	{
-		printf("%d\n", (argv[1] + '0') * (argv[2] + '0'));
+		printf("%d\n", n * m);
 		return (0);
 	}
 	else

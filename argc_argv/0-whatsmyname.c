95a687f362ae89b65998cb4499e06d9562d2fdca
RaptorZ98 <federicoheuercor@hotmail.com>
2023-03-07 18:59:03 -0800

    added script 0

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -8,8 +8,9 @@
  * Return: void
  */
 
-int __argc__ ((unused)) main(int argc, char **argv) { argc }
+int main(int argc, char **argv)
 {
-	printf("%s\n", argv[0]);
+	if (argc > 0)
+		printf("%s\n", argv[0]);
 	return (0);
 }

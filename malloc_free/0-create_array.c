e53735fece977f9e40ba715e2354b3ad510cc02e
RaptorZ98 <federicoheuercor@hotmail.com>
2023-03-13 08:04:00 -0700

    added funct 1

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * create_array - creates an array
+ * @size: param 1
+ * @c: param 2
+ * Return: char
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	char *s;
+	int i;
+
+	if (size == 0)
+		return (NULL);
+	s = malloc(size);
+	if (s == NULL)
+		retrun (NULL);
+	while (n < size)
+	{
+		s[n] = c;
+		n++;
+	}
+	return (s);
+}

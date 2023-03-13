111c28726f9aa5d501d48ec96bbaa014cf0d52d5
RaptorZ98 <federicoheuercor@hotmail.com>
2023-03-13 15:39:39 -0700

    added script 4

---
+++ malloc_free/4-free_grid.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * free_grid - frees a 2d array
+ * @grid: param 1
+ * @height: param 2
+ * Return: void
+ */
+
+void free_grid(int **grid, int height)
+{
+	int n;
+
+	for (n = 0; n < height; n++)
+	{
+		free(grid[n]);
+	}
+	free[grid];
+}

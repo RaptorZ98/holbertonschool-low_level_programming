f5e5b7e4cbd409c5b61a9e1f33d9aedd7d953930
RaptorZ98 <federicoheuercor@hotmail.com>
2023-03-13 20:28:05 -0700

    m13

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,4 +12,28 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+	char *s;
+	unsigned int i, m, d, t;
+
+	i = 0;
+	m = 0;
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+	while (s1[i] != '\0')
+		i++;
+	while (s2[m] != '\0')
+		m++;
+	if (n >= m)
+		n = m;
+	s = malloc(sizeof(char) * i + n + 1);
+	if (s == NULL)
+		return (NULL);
+	for (d = 0; d < i; x++)
+		s[d] = s1[d];
+	for (y = 0; y < n; d++, y++)
+		s[d] = s2[y];
+	s[d] = '\0';
+	return (s);
 }

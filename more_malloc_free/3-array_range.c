ff0e16876292da5a0cf4639b922c1541b42440bc
RaptorZ98 <federicoheuercor@hotmail.com>
2023-03-13 20:11:35 -0700

    added 3

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -18,11 +18,11 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	s = malloc(max * sizeof(int));
+	s = malloc((max - min + 1) * sizeof(int));
 	if (s == NULL)
 		return (NULL);
 	t = min;
-	for (i = 0; i <= max - min; i++)
+	for (i = 0; i <= max - min + 1; i++)
 	{
 		s[i] = t;
 		t++;

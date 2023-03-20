013803927ef1a8868f0e3f6fb97cd8e24694bc8a
RaptorZ98 <federicoheuercor@hotmail.com>
2023-03-20 16:06:12 -0700

    added dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,5 +1,13 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - dog
+ * @name: param 1
+ * @age: param 2
+ * @owner: param 3
+ */
+
 struct dog
 {
 	char *name;

acb0248299ae3cf696dca9f39451144533462912
RaptorZ98 <federicoheuercor@hotmail.com>
2023-03-20 19:09:08 -0700

    add 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,13 +12,15 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	struct dog *new_dog;
+	dog_t *newd;
 
-	new_dog = malloc(sizeof(struct dog));
+	newd = malloc(sizeof(dog_t));
 	if (new_dog == NULL)
 		return (NULL);
-	new_dog->name = name;
+	if (name != NULL)
+		new_dog->name = name;
 	new_dog->age = age;
-	new_dog-> = owner;
-	return (new_dog);
+	if (owner != NULL)
+		new_dog-> = owner;
+	return (newd);
 }

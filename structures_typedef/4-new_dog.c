a43e79da054dced3f1a64ceaac584e19fa0729b1
RaptorZ98 <federicoheuercor@hotmail.com>
2023-03-20 19:35:17 -0700

    added 4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,14 +13,46 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *newd;
+	char *cname;
+	char *cowner;
 
 	newd = malloc(sizeof(dog_t));
 	if (newd == NULL)
 		return (NULL);
 	if (name != NULL)
+	{
+		newd->name = copy(name);
+		if (name == NULL)
+		{
+			free(newd);
+			return (NULL);
+		}
 		newd->name = name;
+	}
 	newd->age = age;
 	if (owner != NULL)
-		newd->owner = owner;
+	{
+		newd->owner = copy(owner);
+		if (owner == NULL)
+		{
+			free(newd);
+			return(NULL);
+		}
+	}
 	return (newd);
+}
+
+char *copy(char *cp)
+{
+	int n, d;
+	char *copy;
+
+	i = 0;
+	while (cp[i] != '\0')
+		i++;
+	copy = malloc(sizeof(char) * (i + 1));
+	if (copy != NULL)
+		for (j = 0; j < (i + 1); j++)
+			copy[j] = cp[j];
+	return (copy);
 }

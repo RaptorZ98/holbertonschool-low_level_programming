7dde5d619a0c2a64542f907a10b434297f1b9f83
RaptorZ98 <federicoheuercor@hotmail.com>
2023-03-21 15:40:05 -0700

    added structure for 3

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,14 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * get_op_func - selects correct function
+ * @s: param 1
+ * Return: int
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,63 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * op_add - adds two int
+ * @a: param 1
+ * @b: param 2
+ * Return: int
+ */
+
+int op_add(int a, int b)
+{
+
+}
+
+/**
+ * op_sub - subs two int
+ * @a: param 1
+ * @b: param 2
+ * Return: int
+ */
+
+int op_sub(int a, int b)
+{
+
+}
+
+/**
+ * op_mul - mult two int
+ * @a: param 1
+ * @b: param 2
+ * Return: int
+ */
+
+int op_mul(int a, int b)
+{
+
+}
+
+/**
+ * op_div - divs two int
+ * @a: param 1
+ * @b: param 2
+ * Return: int
+ */
+
+int op_div(int a, int b)
+{
+
+}
+
+/**
+ * op_mod - mod two int
+ * @a: param 1
+ * @b: param 2
+ * Return: int
+ */
+
+int op_mod(int a, int b)
+{
+
+}

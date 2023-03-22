4ab84b0f4cf66f04f7e1fc6cde9e43e174a6ad4a
RaptorZ98 <federicoheuercor@hotmail.com>
2023-03-21 19:33:26 -0700

    added mistake

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -16,14 +16,20 @@ int main(int argc, char *argv[])
 	int (*f)(int, int);
 
 	if (argc != 4)
+	{
+		printf("Error\n");
 		exit(98);
+	}
 
 	i = atoi(argv[1]);
 	n = atoi(argv[3]);
 
 	f = get_op_func(argv[2]);
 	if (f == NULL)
+	{
+		printf("Error\n");
 		exit(99);
+	}
 	printf("%d\n", f(i, n));
 	return (0);
 }

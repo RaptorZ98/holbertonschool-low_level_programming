1d2c30a79b989bd754249f3f276dcab7b5a9b5e2
RaptorZ98 <federicoheuercor@hotmail.com>
2023-03-21 19:34:29 -0700

    mistake

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -48,8 +48,10 @@ int op_mul(int a, int b)
 int op_div(int a, int b)
 {
 	if (b == 0)
+	{
 		printf("Error\n");
 		exit(100);
+	}
 	return (a / b);
 }
 
@@ -63,7 +65,9 @@ int op_div(int a, int b)
 int op_mod(int a, int b)
 {
 	if (b == 0)
+	{
 		printf("Error\n");
 		exit(100);
+	}
 	return (a % b);
 }

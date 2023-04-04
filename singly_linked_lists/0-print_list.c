108c8226e5d5c7fe089d6ef56b4ce8fa58541da3
RaptorZ98 <federicoheuercor@hotmail.com>
2023-04-04 09:10:38 -0700

    mistakes in nil and the case of h NULL

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -8,10 +8,12 @@
 
 size_t print_list(const list_t *h)
 {
+	if (h == NULL)
+		return (0);
 	if (h->next != NULL)
 	{
 		if (h->str == NULL)
-			printf("(nil)");
+			printf("(nil)\n");
 		else
 			printf("[%d] %s\n", h->len, h->str);
 		return (1 + print_list(h->next));

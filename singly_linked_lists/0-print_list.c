c995421c2686efe2517fa0589cfd609cc5b62aef
RaptorZ98 <federicoheuercor@hotmail.com>
2023-04-04 09:08:33 -0700

    changed the printing of nil

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -11,8 +11,9 @@ size_t print_list(const list_t *h)
 	if (h->next != NULL)
 	{
 		if (h->str == NULL)
-			h->str = "(nil)";
-		printf("[%d] %s\n", h->len, h->str);
+			printf("(nil)");
+		else
+			printf("[%d] %s\n", h->len, h->str);
 		return (1 + print_list(h->next));
 	}
 	printf("[%d] %s\n", h->len, h->str);

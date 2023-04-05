d031b1c928d74f33e9a280496bcc78dc2e6b8d94
RaptorZ98 <federicoheuercor@hotmail.com>
2023-04-04 18:56:33 -0700

    mistakes corrected

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -18,7 +18,7 @@ list_t *add_node_end(list_t **head, const char *str)
 		return (NULL);
 	if (strCopy == NULL)
 		return (NULL);
-	while (strCopu[length] != NULL)
+	while (strCopy[length] != '\0')
 		length++;
 	new = malloc(sizeof(list_t));
 	if (new == NULL)

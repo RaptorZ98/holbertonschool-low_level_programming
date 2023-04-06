7f64998f5e6972a7cafed5caa10d9c513c4d4bde
RaptorZ98 <federicoheuercor@hotmail.com>
2023-04-06 04:43:11 -0700

    added function 0

---
+++ doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all elements of a dlistint
+ * @h: a pointer to the head of the list
+ * Return: the number of nodes
+ */
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	if (h == NULL)
+		return (0);
+	if (h->next == NULL)
+	{
+		printf("%d\n", h->n);
+		return (1);
+	}
+	return (print_dlistint(h->next));
+}

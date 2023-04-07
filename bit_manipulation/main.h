6624a75757217384c2540eb002bec8e097b9a1cd
RaptorZ98 <federicoheuercor@hotmail.com>
2023-04-06 21:48:51 -0700

    corrected betty mistakes

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,6 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
 unsigned int power_of(unsigned int n);
 
 unsigned int binary_to_uint(const char *b);

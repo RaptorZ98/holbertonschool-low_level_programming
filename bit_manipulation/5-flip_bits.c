42cb30db389fc416f668c61609ef272faf6404b8
RaptorZ98 <federicoheuercor@hotmail.com>
2023-04-07 16:09:32 -0700

    added function 5

---
+++ bit_manipulation/5-flip_bits.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * flip_bits - retrun bits changed to make another number
+ * @n: first number to compare
+ * @m: seconf number to compare
+ * Return: the number of bits to change
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	unsigned long int num = 1;
+	int result = 0, lup;
+
+	num << 62;
+	for (lup = 0; lup <= 62; lup++)
+	{
+		if (((n & num) != 0) && ((m & num) != 0))
+		{
+		}
+		else
+			result++;
+		num >> 1;
+	}
+	return (result);
+}

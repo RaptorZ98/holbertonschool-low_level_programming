5715948fab55321d823c31adf82b3f594162c55e
RaptorZ98 <federicoheuercor@hotmail.com>
2023-04-07 15:40:24 -0700

    added function 3

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * set_bit - sets the value of a bit to 1 at a given index
+ * @n: integer to change
+ * @index: the index to change in n
+ * Return: 1 if it worked, or -1 if not
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	if (index >= 63)
+		return (-1);
+	n = (n | (1 << index));
+	return (0);
+}

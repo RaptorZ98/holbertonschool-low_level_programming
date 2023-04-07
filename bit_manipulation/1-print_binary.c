a64aab86370255449b6e6f42bbb2e286aa40652b
RaptorZ98 <federicoheuercor@hotmail.com>
2023-04-07 12:36:22 -0700

    mistake int insted of long int

--- bit_manipulation/1-print_binary.c
+++ bit_manipulation/1-print_binary.c
@@ -8,11 +8,14 @@
 void print_binary(unsigned long int n)
 {
 	int check = 0, i;
-	unsigned int mask;
+	unsigned long int mask;
 
 	mask = 2147483648;
 	if (n == 0)
+	{
 		_putchar('0');
+		return;
+	}
 	for (i = 0; i <= 33; i++)
 	{
 		if ((n & mask) != 0)

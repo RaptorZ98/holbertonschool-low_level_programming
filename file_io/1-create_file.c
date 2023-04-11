30d3d99a4afcca56db1f85864cfea5f8b00713b5
RaptorZ98 <federicoheuercor@hotmail.com>
2023-04-11 09:39:29 -0700

    mistake in the syntax of open

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -14,7 +14,7 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 
-	file = open(filename,O_TRUNC , O_CREAT, 00600);
+	file = open(filename, O_RDWR | O_TRUNC | O_CREAT, 00600);
 	if (file == -1)
 		return (-1);
 

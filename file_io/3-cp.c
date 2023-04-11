bb392316c341bc9ba1a48ba42f89fdb66aa19831
RaptorZ98 <federicoheuercor@hotmail.com>
2023-04-11 10:30:07 -0700

    added incomplete function 3

---
+++ file_io/3-cp.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - copies the content of a file to another file
+ * @argc: the 
+ * @argv: the
+ * Return:
+ */
+
+ int main(int argc, char *argv[])
+ {
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to");
+		error(97);
+	}
+	if 
+ }

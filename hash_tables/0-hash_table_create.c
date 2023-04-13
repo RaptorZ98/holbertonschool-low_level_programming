6cabd7abde0f65e4761b7b63c7f1faff972a9d84
RaptorZ98 <federicoheuercor@hotmail.com>
2023-04-12 18:26:05 -0700

    added function 0 hash tables

---
+++ hash_tables/0-hash_table_create.c
@@ -0,0 +1,28 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - creates a hash table
+ * @size: the size of the table
+ * Return: the adress of the created hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+
+	hash_table_t *table;
+	hash_node_t *arr;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+		return (NULL);
+	arr = malloc(sizeof(hash_node_t) * size);
+	if (arr == NULL)
+	{
+		free(table);
+		return(NULL);
+	}
+
+	table.size = size;
+	table.array = &arr;
+
+	return (table);
+}

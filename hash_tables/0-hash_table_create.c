3d214186f44302035034e400e7dc18576e6c96cf
RaptorZ98 <federicoheuercor@hotmail.com>
2023-04-13 20:49:58 -0700

    m

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -9,25 +9,19 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *table;
-	hash_node_t *arr;
 	unsigned long int pos;
 
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
-	arr = malloc(sizeof(hash_node_t *) * size);
+	table->array = malloc(sizeof(hash_node_t *) * size);
 	if (arr == NULL)
 	{
 		free(table);
 		return (NULL);
 	}
-	else
-	{
-		table->array = &arr;
-		for (pos = 0; pos < size; pos++)
-			table->array[pos] = NULL;
-	}
-
+	for (pos = 0; pos < size; pos++)
+		table->array[pos] = NULL;
 	table->size = size;
 
 	return (table);

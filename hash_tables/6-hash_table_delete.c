d5fb63365aa397b0302127d2ee38f2bb04f86d4b
RaptorZ98 <federicoheuercor@hotmail.com>
2023-04-13 21:36:16 -0700

    added function 6

---
+++ hash_tables/6-hash_table_delete.c
@@ -0,0 +1,30 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - deletes a hash table
+ * @ht: the hash table to delete
+ */
+
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int pos, len;
+	hash_node_t *tmp;
+
+	if (ht == NULL)
+		return;
+
+	for (pos = 0; pos < ht->size; pos++)
+	{
+		while (ht->array[pos] != NULL)
+		{
+			tmp = ht->array[pos]->next;
+			free(ht->array[pos]->value);
+			free(ht->array[pos]->key);
+			free(ht->array[pos]);
+			ht->array[pos] = tmp;
+		}
+		pos++;
+	}
+	free(ht->array);
+	free(ht);
+}

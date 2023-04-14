2675e0629bb7d236ec838fd6107c0b49789a4296
RaptorZ98 <federicoheuercor@hotmail.com>
2023-04-13 20:50:58 -0700

    m

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -15,7 +15,7 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (table == NULL)
 		return (NULL);
 	table->array = malloc(sizeof(hash_node_t *) * size);
-	if (arr == NULL)
+	if (table->array == NULL)
 	{
 		free(table);
 		return (NULL);

2a2b0176bcee6a5599ff099119613f2482d1bc9c
RaptorZ98 <federicoheuercor@hotmail.com>
2023-04-13 22:14:06 -0700

    m

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -25,7 +25,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	}
 	else if (strcmp(ht->array[indexKey]->key, key) == 0)
 	{
-		ht->array[indexKey]->value = strdup(value);
+		strcpy(ht-array[indexKey]->value, value);
 		return (1);
 	}
 	else

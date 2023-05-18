64c9a5c5c81cd9803272b439f129c3f4fcb1b62e
RaptorZ98 <federicoheuercor@hotmail.com>
2023-05-18 11:10:51 -0700

    corrected betty mistakes

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -4,6 +4,7 @@
  * linear_search - searches for a value in array using linear search algorthm
  * @array: the array to search for the value
  * @value: the value to search for
+ * @size: the size of the array
  * Return: the first index where value is located, if not located -1
 */
 int linear_search(int *array, size_t size, int value)

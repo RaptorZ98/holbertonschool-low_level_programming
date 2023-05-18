965324dea4dc89de1cc766791f09f1c40dd74d8b
RaptorZ98 <federicoheuercor@hotmail.com>
2023-05-18 11:08:34 -0700

    added function 0

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,28 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in array using linear search algorthm
+ * @array: the array to search for the value
+ * @value: the value to search for
+ * Return: the first index where value is located, if not located -1
+*/
+int linear_search(int *array, size_t size, int value)
+{
+	size_t len = 0;
+	int result = -1;
+
+	if (array == NULL)
+		return (-1);
+	while (len < size)
+	{
+		printf("Value checked array[%d] = [%d]\n", len, array[len]);
+		if (array[len] == value)
+		{
+			result = len;
+			break;
+		}
+		len++;
+	}
+	printf("Found %d at index: %d\n", result);
+	return (result);
+}
